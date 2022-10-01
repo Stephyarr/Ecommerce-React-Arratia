064f439e1524a779a49b863d36d99c823ee9ba1d
Stephyarr <stephaniearratia21@gmail.com>
2022-10-01 00:53:01 -0300

    modi-navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { style } from './NavBar.style'
 
 const NavBar = () => {
   return (
     <header style={style.container}>
-      <h1>Lions</h1>
+      <h1 style={style.title}>Lions</h1>
       <nav>
         <a style={style.navbar} href="/shop">Productos</a>
         <a style={style.navbar} href="/Nosotros">Nosotros</a>
@@ -18,23 +19,5 @@ const NavBar = () => {
 
 export default NavBar
 
-const style = {
-  container:{
-    display: 'flex',
-    height: 60,
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    backgroundImage: 'linear-gradient(to top, #fbc2eb 0%, #a6c1ee 100%)',
-  },
-  navbar:{
-    margin: 10,
-    fontSize: 18,
-    textDecoration: 'none',
-    color: 'black'
-  },
-  icons:{
-    fontSize: 25,
-    margin: 10
-  }
-}
+
 
---
+++ src/components/NavBar/NavBar.style.js
@@ -0,0 +1,23 @@
+export const style = {
+    container:{
+      display: 'flex',
+      height: 60,
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      backgroundImage: 'linear-gradient(to top, #fbc2eb 0%, #a6c1ee 100%)',
+    },
+    title:{
+      margin: 10,
+  
+    },
+    navbar:{
+      margin: 10,
+      fontSize: 18,
+      textDecoration: 'none',
+      color: 'black'
+    },
+    icons:{
+      fontSize: 25,
+      margin: 10
+    }
+  }
